e1c6ec477d9e2a5bd269808f5cf717f4606396f8
g.lewarne <g.lewarne@gmx.co.uk>
2012-12-28 17:06:35 +0000

    Initial push of Samsung - GT-N7102 JB opensource

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 SMP PREEMPT Wed Oct 24 13:13:42 KST 2012"
+#define LINUX_COMPILE_BY "jungkilim"
+#define LINUX_COMPILE_HOST "jungkilim-desktop"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
